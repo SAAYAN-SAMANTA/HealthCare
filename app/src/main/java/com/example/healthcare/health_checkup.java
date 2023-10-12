e140ff93b26a6206bebfd87421d561c730099eec
SAAYAN-SAMANTA <saayansamanta2003@gmail.com>
2023-10-12 09:34:01 +0530

    updated

--- app/src/main/java/com/example/healthcare/health_checkup.java
+++ app/src/main/java/com/example/healthcare/health_checkup.java
@@ -24,16 +24,16 @@ EditText blood_pressure;
         startActivity(intent);
     }
 
-//    public void examin(View view){
-//        String check_pressure=blood_pressure.getText().toString();
-//        int systolic_bp= Integer.parseInt(check_pressure.substring(check_pressure.indexOf('/')+1));
-//        int diastolic_bp=Integer.parseInt(check_pressure.substring(check_pressure.indexOf('/'),(check_pressure.length()+1)));
-//        if(systolic_bp<=90 && diastolic_bp<=60){
-//            Toast.makeText(health_checkup.this,"low bp",Toast.LENGTH_SHORT).show();
-//        } else if (systolic_bp<=120 && diastolic_bp==60) {
-//            Toast.makeText(health_checkup.this,"Normal bp",Toast.LENGTH_SHORT).show();
-//        } else{
-//            Toast.makeText(health_checkup.this,"high bp",Toast.LENGTH_SHORT).show();
-//        }
-//    }
+    public void examin(View view){
+        String check_pressure=blood_pressure.getText().toString();
+        int systolic_bp= Integer.parseInt(check_pressure.substring(0,check_pressure.indexOf('/')+1));
+        int diastolic_bp=Integer.parseInt(check_pressure.substring(check_pressure.indexOf('/'),(check_pressure.length()+1)));
+        if(systolic_bp<=90 && diastolic_bp<=60){
+            Toast.makeText(health_checkup.this,"low bp",Toast.LENGTH_SHORT).show();
+        } else if (systolic_bp<=120 && diastolic_bp==60) {
+            Toast.makeText(health_checkup.this,"Normal bp",Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(health_checkup.this,"high bp",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
