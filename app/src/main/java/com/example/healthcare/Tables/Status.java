222022f796066722f83a3841b637ce3b8c0e864a
Debasis Nishank <95074176+debasisnishank@users.noreply.github.com>
2023-11-12 10:08:40 +0530

    Updated Status.java

--- app/src/main/java/com/example/healthcare/Tables/Status.java
+++ app/src/main/java/com/example/healthcare/Tables/Status.java
@@ -1,1 +1,23 @@
+package com.example.healthcare.Tables;
+
+public class Status {
+    private long id;
+    private String statusText;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStatusText() {
+        return statusText;
+    }
+
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
+    }
+}
 
