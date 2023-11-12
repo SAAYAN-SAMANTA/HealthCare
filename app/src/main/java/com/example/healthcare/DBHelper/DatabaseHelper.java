610214327c0a159edfc12e409f57a20efb8f430f
Debasis Nishank <95074176+debasisnishank@users.noreply.github.com>
2023-11-12 10:03:35 +0530

    Updated DatabaseHelper.java

--- app/src/main/java/com/example/healthcare/DBHelper/DatabaseHelper.java
+++ app/src/main/java/com/example/healthcare/DBHelper/DatabaseHelper.java
@@ -1,4 +1,38 @@
-package com.example.healthcare.DBHelper;
+package com.example.healthcare.Data;
 
-public class DatabaseHelper {
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.healthcare.DBHelper.DatabaseHelper;
+import com.example.healthcare.Tables.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatusDataSource {
+    private SQLiteDatabase database;
+    private DatabaseHelper dbHelper;
+
+    public StatusDataSource(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public void open() {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long saveStatus(String statusText) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_STATUS, statusText);
+        return database.insert(DatabaseHelper.TABLE_STATUS, null, values);
+    }
+
+  
 }
