37c200a63cfe5ce6c8da13bcf4baacd3c9e1dc1f
Debasis Nishank <95074176+debasisnishank@users.noreply.github.com>
2023-11-12 10:00:21 +0530

    Updated methods in StatusDataSource.java

--- app/src/main/java/com/example/healthcare/Data/StatusDataSource.java
+++ app/src/main/java/com/example/healthcare/Data/StatusDataSource.java
@@ -16,6 +16,57 @@ import java.util.List;
 public class StatusDataSource {
     private SQLiteDatabase database;
     private DatabaseHelper dbHelper;
+   public StatusDataSource(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
 
+    public void open() {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long saveStatus(String statusText) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_STATUS, statusText);
+        return database.insert(DatabaseHelper.TABLE_STATUS, null, values);
+    }
+
+    public List<Status> getAllStatus() {
+        List<Status> statusList = new ArrayList<>();
+        Cursor cursor = database.query(
+                DatabaseHelper.TABLE_STATUS,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Status status = cursorToStatus(cursor);
+            statusList.add(status);
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        return statusList;
+    }
+
+    @SuppressLint("Range")
+    private Status cursorToStatus(Cursor cursor) {
+        Status status = new Status();
+        status.setId(cursor.getLong(cursor.getColumnIndex(DatabaseHelper.COLUMN_ID)));
+        status.setStatusText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_STATUS)));
+        return status;
+    }
+
+    public void deleteAllHealthStatus() {
+        database.delete(DatabaseHelper.TABLE_STATUS, null, null);
+    }
  
 }
