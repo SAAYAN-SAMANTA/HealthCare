ab7ef6779b4e50251f91e42905e327f72d0f9a41
SUSANTA KUMAR DEBATA <142401059+SusantaDebata@users.noreply.github.com>
2023-11-13 21:13:23 +0530

    Users.java is completed

---
+++ app/src/main/java/com/example/healthcare/Tables/Users.java
@@ -0,0 +1,94 @@
+package com.example.healthcare.Tables;
+
+public class Users {
+    private long id;
+    private String name;
+    private long age;
+    private  String gender;
+    private String address;
+    private String email;
+    private long contact;
+    private  String role;
+    private String username;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getContact() {
+        return contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
